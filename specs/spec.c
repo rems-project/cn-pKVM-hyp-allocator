347069239a2de32edaa24afde542b8013b63842a
pes20 <Peter.Sewell@cl.cam.ac.uk>
2025-03-19 19:52:20 +0000

    eyeball heap allocator - add a few comments to the spec file

--- specs/spec.c
+++ specs/spec.c
@@ -6,6 +6,12 @@ chunk:
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^  mapped_size
 **/
 
+/*PS: `chunk_is_used` suggests that chunks can be unused, iff size=0.  But probably only transiently?  No, probably not - see the chunk_try_destroy comment.*/
+
+/*PS: what are the hashes used for?  AIUI, to fail fast if the chunk header got stomped on by bad user code.  They cover the next/prev pointers so have to be updated whenever the list structure changes.  So not stable, so presumably not exposed to clients. Could be removed if the clients were persistently verified to be memory safe */
+
+/*PS: in addition to our first sketch, the spec should constrain what is actually mapped - by making the underlying __hyp_allocator_map and pkvm_remove_mappings in shim.c have specs that record that as finite maps (and fail if something tries to map illegally).  And we need to spec the memcache operations. */
+
 type_synonym va = u64
   
 type_synonym cn_chunk_hdr = {
@@ -19,6 +25,9 @@ datatype cn_chunk_hdrs {
   Chunk_nil {};
   Chunk_cons { cn_chunk_hdr hd; datatype cn_chunk_hdrs tl; };
 }
+
+/*PS: if we're abstracting the chunks to a CN custom list as above, then we'll abstract a `struct chunk_hdr *chunk` to a natural-number index into that list and define an `nth`?  Or add the actual address to the abstraction and search for that to access?  Think we need that address in any case, though that doesn't decide this. */
+
 
 /* invoke as cn_chunk_hdrs(ha.chunks.next, &(ha.chunks.next), ha.chunks.prev)*/
 predicate Cn_chunk_hdrs( pointer p, pointer prev, pointer last ) {
