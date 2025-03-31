ff53cb3933dcc03b1f680a44a12e7eff17b97802
Rini Banerjee <rb2018@cam.ac.uk>
2025-03-31 14:44:28 +0100

    Make spec.c closer to real CN syntax

--- specs/spec.c
+++ specs/spec.c
@@ -12,57 +12,92 @@ chunk:
 
 /*PS: in addition to our first sketch, the spec should constrain what is actually mapped - by making the underlying __hyp_allocator_map and pkvm_remove_mappings in shim.c have specs that record that as finite maps (and fail if something tries to map illegally).  And we need to spec the memcache operations. */
 
+#include <inttypes.h>
+
+typedef uint8_t		u8;
+typedef uint32_t	u32;
+typedef uint64_t	u64;
+
+typedef u64		phys_addr_t;
+
+// This is defined in include/linux/types.h
+struct list_head {
+	struct list_head *next, *prev;
+};
+
+// TODO: spinlock
+typedef u64		hyp_spinlock_t;
+
+static struct hyp_allocator {
+	struct list_head	chunks;
+	unsigned long		start;
+	u32			size;
+	hyp_spinlock_t		lock;
+} hyp_allocator;
+
+struct chunk_hdr {
+	u32			alloc_size;
+	u32			mapped_size;
+	struct list_head	node;
+	u32			hash;
+	char			data /* __aligned(8) */;
+};
+
+/*@
 type_synonym va = u64
   
 type_synonym cn_chunk_hdr = {
-  va header_address;/* the VA of the start of the chunk_hdr*/
-  u32 alloc_size;   /* exactly as in the C */
-  u32 mapped_size;  /* exactly as in the C */
-  /* no node, no hash, no data, no ownership here */
+  va header_address, // the VA of the start of the chunk_hdr
+  u32 alloc_size, // exactly as in the C 
+  u32 mapped_size  // exactly as in the C 
+  // no node, no hash, no data, no ownership here
 }
 
 datatype cn_chunk_hdrs {
-  Chunk_nil {};
-  Chunk_cons { cn_chunk_hdr hd; datatype cn_chunk_hdrs tl; };
+  Chunk_nil {},
+  Chunk_cons { cn_chunk_hdr hd, datatype cn_chunk_hdrs tl }
 }
+@*/
 
 /*PS: if we're abstracting the chunks to a CN custom list as above, then we'll abstract a `struct chunk_hdr *chunk` to a natural-number index into that list and define an `nth`?  Or add the actual address to the abstraction and search for that to access?  Think we need that address in any case, though that doesn't decide this. */
 
 
 /* invoke as cn_chunk_hdrs(ha.chunks.next, &(ha.chunks.next), ha.chunks.prev)*/
 /* start and end are the va space within which the chunks have to be */
-predicate Cn_chunk_hdrs( pointer p, pointer prev, pointer last, va start, va end ) {
+/*@
+predicate (datatype cn_chunk_hdrs) Cn_chunk_hdrs( pointer p, pointer prev, pointer last, va start, va end ) {
   if (p==last) 
-    { start <= end;
+    { assert(start <= end);
       return Chunk_nil {}; }
   else
      { 
        let header_address = container_of(p); // or some offsetof arithmetic 
        take hdr = Owned<struct chunk_hdr>(header_address);
        let cn_hdr = {
-         header_address : (va)header_address;
-         alloc_size : hdr.alloc_size;
-         mapped_size : hdr.mapped_size;
+         header_address : (u64) header_address,
+         alloc_size : hdr.alloc_size,
+         mapped_size : hdr.mapped_size
        };
        
-       /* check non-overlappingness */
-       cn_hdr.header_address >= start;
+       // check non-overlappingness
+       assert(cn_hdr.header_address >= start);
        let chunk_end = cn_hdr.header_address + cn_hdr.mapped_size;
-       chunk_end <= end;
+       assert(chunk_end <= end);
 
-       /* ownership of the mapped but not allocated part of the chunk - as [from...to) in u64 va */
-       take _ = Cn_char_array((va)header_address + sizeof(struct chunk_hdr) + hdr.alloc_size, (va)header_address + hdr.mapped_size);
-       /* the tail of the list */
+       // ownership of the mapped but not allocated part of the chunk - as [from...to) in u64 va
+       take A = Cn_char_array((u64)header_address + sizeof<struct chunk_hdr> + hdr.alloc_size, (u64)header_address + hdr.mapped_size);
+       // the tail of the list 
        take tl = Cn_chunk_hdrs(hdr.node.next, p, last, chunk_end, end);
-       /* do we want to use resources to track the va-address-space "ownership" of any unmapped part of va address space between this chunk and the next (or the end)? unclear. pretend not for now */
-       return Chunk_cons { hd: cn_hdr; tl: tl; };
+       // do we want to use resources to track the va-address-space "ownership" of any unmapped part of va address space between this chunk and the next (or the end)? unclear. pretend not for now
+       return Chunk_cons { hd: cn_hdr, tl: tl };
      }
 }
 
                             
-predicate Cn_hyp_allocator( pointer p /* struct hyp_allocator* */) {
+predicate ({struct hyp_allocator ha, datatype cn_chunk_hdrs hdrs}) Cn_hyp_allocator( pointer p  ) { // p points to a struct hyp_allocator
   take ha = Owned<struct hyp_allocator>(p);
   take hdrs = Cn_chunk_hdrs(ha.chunks.next, &(ha.chunks.next), ha.chunks.prev);
-  return( {ha:ha; hdrs:hdrs} );
-  /* morally on initialisation this owns all the va space that isn't in the chunks - but we're not currently representing "va ownership" with ownership.  So there is no extra ownership on initialisation - that's all in the memcache */
+  return( {ha:ha, hdrs:hdrs} );
+  // morally on initialisation this owns all the va space that isn't in the chunks - but we're not currently representing "va ownership" with ownership.  So there is no extra ownership on initialisation - that's all in the memcache
 }
+@*/
