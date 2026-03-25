7f8455102b94dee78a5c6ef4b081059cc5b61cc7
Hiroyuki Katsura <moratorium08@gmail.com>
2026-03-25 18:47:24 +0000

    Revert "[Claude] Add benchmark infrastructure with switch_to, run_bench, and run_fulminate scripts"
    This reverts commit 3c7c5f3b4cbcc922aae6be48ab0da614dddbe40f.

--- benchmark/run_fulminate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Usage: ./benchmark/run_fulminate.sh <benchmark_number>
-
-make main.exe > /dev/null 2>&1 && ./main.exe > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-    echo "Benchmark $1: OK"
-else
-    echo "Benchmark $1: NG"
-fi
