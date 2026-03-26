8a3e98dd062dc15c534e1d80bc7d134c143c62d4
Rini Banerjee <rbanerjee20@gmail.com>
2026-03-26 16:32:23 +0000

    Instrumented time and space reported

--- fulm_bench/fulm_bench.py
+++ fulm_bench/fulm_bench.py
@@ -40,18 +40,29 @@ def compile(filename):
     run_cmd(compile + filename)
 
 def link(filename, instrumented):
-    link = cc + " " + filename + " /Users/rinibanerjee/.opam/default/lib/cn/runtime/libcn_exec.a -L " + runtime_prefix + " -lcn_exec -o main" + ".instrumented" if instrumented else "" + ".exe"
+    link = cc + " " + filename + " /Users/rinibanerjee/.opam/default/lib/cn/runtime/libcn_exec.a -L " + runtime_prefix + " -lcn_exec -o main" + (".instrumented" if instrumented else "") + ".exe"
     run_cmd(link)
 
-def run(instrumented):
-    run = "./main" + ".instrumented" if instrumented else "" + ".exe"
-    run_cmd(run)
+def run(instrumented, time=False):
+    run = (gtime if time else "") + "./main" + (".instrumented" if instrumented else "") + ".exe"
+    print(run)
+    res = subprocess.run(run.split(), capture_output=True, text = True)
+    output = res.stderr
+    if time:
+        return output.split('~')[-2:]
 
 
-pp_f_prefix = "main.pp.bench.new"
+pp_f_prefix = "main.pp.bench"
 pp_f_name = pp_f_prefix + ".c"
 
 preprocess(pp_f_name)
+# compile(pp_f_name)
+# pp_f_obj_file = pp_f_prefix + ".o"
+# link(pp_f_obj_file, instrumented=False)
+# for i in range(1):
+#     run(instrumented=False, time=True)
+
+
 fulminate(pp_f_name)
 
 pp_f_exec_prefix = pp_f_prefix + ".exec"
@@ -59,7 +70,19 @@ pp_f_exec_name = pp_f_exec_prefix + ".c"
 
 compile(pp_f_exec_name)
 
-pp_f_obj_file = pp_f_exec_prefix + ".o"
+pp_f_exec_obj_file = pp_f_exec_prefix + ".o"
+link(pp_f_exec_obj_file, instrumented=True)
+# run(instrumented=True)
 
-link(pp_f_obj_file, instrumented=True)
-run(instrumented=True)
+instr_times = []
+instr_spaces = []
+for i in range(11):
+    time, space = run(instrumented=True, time=True)
+    instr_times.append(time)
+    instr_spaces.append(space)
+
+median_instr_time = statistics.median(instr_times)
+median_instr_space = statistics.median(instr_spaces)
+
+print(statistics.median(instr_times))
+print(statistics.median(instr_spaces))
