e60d9f4a22f0ec1e875c8634f434dced9ed84ec4
Rini Banerjee <rbanerjee20@gmail.com>
2026-03-26 23:43:11 +0000

    Remove iteration over independent variable to hyp alloc

--- fulm_bench/uninstr_bench.py
+++ fulm_bench/uninstr_bench.py
@@ -14,16 +14,29 @@ def run_cmd(cmd):
     # print(res.split(sep='~'))
     return res.split('~')[1:]
 
+def find_and_replace_macro(f, input_basename, new_str):
+    # Assume there is a macro of the form #define SIZE magic in the input file
+    with open(f, 'r') as file:
+        filedata = file.read()
+
+    filedata = filedata.replace("magic", new_str)
+    subst_f_name = input_basename + ".subst.c"
+
+    with open(subst_f_name, 'w') as file:
+        file.write(filedata)
+
+    return subst_f_name
+
 
 gtime = 'gtime -f ~%e~%M '
 
-for i in range(1, 9):
-    times = []
-    spaces = []
-    for j in range(11):
-        time, space = run_cmd(gtime + "./uninstr/main_" + str(2 ** i))
-        times.append(time)
-        spaces.append(space)
-    print("Number of pages: " + str(2**i))
-    print("Median time: " + str(statistics.median(times)))
-    print("Median space: " + str(statistics.median(spaces)))
+times = []
+spaces = []
+# str_power = str(2**i)
+# find_and_replace_macro("src/main.c", "main_" + str_power, str(2**i))
+for j in range(11):
+    time, space = run_cmd(gtime + "./uninstr/main")
+    times.append(time)
+    spaces.append(space)
+print("Median time: " + str(statistics.median(times)))
+print("Median space: " + str(statistics.median(spaces)))
